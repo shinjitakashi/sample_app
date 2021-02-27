3b4cca92f8dc17abd29b1f9f2f3221b73bb602bd
takashi <electronicgreeeen@gmail.com>
2021-02-28 02:03:49 +0900

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
